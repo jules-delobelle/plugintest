12b34c536f62dab6e718e3b8eeca7f735a446e1d
Jules <jules.delobelle@free.fr>
2024-02-10 15:38:46 +0100

    Added first commands

--- plugintest/src/fr/akaazee/plugintest/Main.java
+++ plugintest/src/fr/akaazee/plugintest/Main.java
@@ -2,15 +2,19 @@ package fr.akaazee.plugintest;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import fr.akaazee.plugintest.commands.CommandTest;
+
 public class Main extends JavaPlugin {
 	
 	@Override
 	public void onEnable() {
 		System.out.println("Le plugin viens de s'allumer");
+		getCommand("test").setExecutor(new CommandTest());
 	}
 	
 	@Override
 	public void onDisable() {
 		System.out.println("Le plugin viens de s'éteindre");
 	}
+	
 }
---
+++ plugintest/src/fr/akaazee/plugintest/commands/CommandTest.java
@@ -0,0 +1,22 @@
+package fr.akaazee.plugintest.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class CommandTest implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String msg, String[] args) {
+		
+		if(sender instanceof Player) {
+			Player player = (Player)sender;
+			player.sendMessage("§eMessage §bTest");
+		}
+		return false;
+	}
+
+}
