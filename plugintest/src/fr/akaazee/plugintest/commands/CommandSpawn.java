6cc1566da1b326d90ccbaa2157bd0ea24f2fab7a
Jules <jules.delobelle@free.fr>
2024-02-11 21:13:48 +0100

    Finished spawn command

--- plugintest/src/fr/akaazee/plugintest/commands/CommandSpawn.java
+++ plugintest/src/fr/akaazee/plugintest/commands/CommandSpawn.java
@@ -1,14 +1,21 @@
 package fr.akaazee.plugintest.commands;
 
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class CommandSpawn implements CommandExecutor {
 
 	@Override
-	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
-		// TODO Auto-generated method stub
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		
+		if(sender instanceof Player) {
+			Player player = (Player)sender;
+			Location spawn = new Location(player.getWorld(), 150, 104, 85, -90f, 0f);
+			player.teleport(spawn);
+		}
 		return false;
 	}
 
